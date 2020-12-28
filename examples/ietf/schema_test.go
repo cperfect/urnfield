70523154bb9e585032f70130a57c0073d065cf0c
Chris Perfect <1636270+cperfect@users.noreply.github.com>
2020-12-28 12:32:24 +1100

    WIP ietf - proper test framework

--- examples/ietf/schema_test.go
+++ examples/ietf/schema_test.go
@@ -3,6 +3,7 @@ package ietf
 import (
 	"testing"
 
+	"github.com/cperfect/urnfield"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,6 +14,18 @@ https://www.ietf.org/download/rfc-index.txt
 https://www.iana.org/assignments/params/params.xml#params-1
 https://www.iana.org/assignments/xml-registry/xml-registry.xhtml
 */
+
+type urnTestData struct {
+	urnfield.Urn
+	desc          string
+	urnString     string
+	shouldSucceed bool
+}
+
+type testSchemaData struct {
+	urnTestData
+	*urnfield.Schema
+}
 
 var (
 	testIetfUrnData = []string{
@@ -25,6 +38,100 @@ var (
 		"urn:ietf:any-string123",                              //anything else
 	}
 )
+
+var testUrnSchemas = []testSchemaData{
+	//"urn:ietf:rfc:4350", //https://www.ietf.org/rfc/rfc4350.html"
+	{
+		urnTestData{
+			urnfield.Urn{
+				Nid: "ietf",
+				Nss: []string{"rfc", "4350"},
+			},
+			"Good rfc",
+			"urn:ietf:rfc:4350",
+			true,
+		},
+		Schema,
+	},
+	//"urn:ietf:fyi:20",   //https://tools.ietf.org/html/fyi20
+	{
+		urnTestData{
+			urnfield.Urn{
+				Nid: "ietf",
+				Nss: []string{"fyi", "20"},
+			},
+			"Good fyi",
+			"urn:ietf:fyi:20",
+			true,
+		},
+		Schema,
+	},
+	//"urn:ietf:std:90",   //https://tools.ietf.org/html/std90
+	{
+		urnTestData{
+			urnfield.Urn{
+				Nid: "ietf",
+				Nss: []string{"std", "90"},
+			},
+			"Good std",
+			"urn:ietf:std:90",
+			true,
+		},
+		Schema,
+	},
+	//"urn:ietf:bcp:13",   //https://tools.ietf.org/html/bcp13
+	{
+		urnTestData{
+			urnfield.Urn{
+				Nid: "ietf",
+				Nss: []string{"bcp", "13"},
+			},
+			"Good bcp",
+			"urn:ietf:bcp:13",
+			true,
+		},
+		Schema,
+	},
+	//"urn:ietf:id:draft-www-opsawg-yang-vpn-service-pm-02", //https://tools.ietf.org/html/draft-www-opsawg-yang-vpn-service-pm-02
+	{
+		urnTestData{
+			urnfield.Urn{
+				Nid: "ietf",
+				Nss: []string{"id", "draft-www-opsawg-yang-vpn-service-pm-02"},
+			},
+			"Good internet draft",
+			"urn:ietf:id:draft-www-opsawg-yang-vpn-service-pm-02",
+			true,
+		},
+		Schema,
+	},
+	//"urn:ietf:params:xml:ns:allocationToken-1.0",          //https://www.iana.org/assignments/xml-registry/ns/allocationToken-1.0.txt
+	{
+		urnTestData{
+			urnfield.Urn{
+				Nid: "ietf",
+				Nss: []string{"params", "xml", "ns", "allocationToken-1.0"},
+			},
+			"Good params (xml ns)",
+			"urn:ietf:params:xml:ns:allocationToken-1.0",
+			true,
+		},
+		Schema,
+	},
+	//"urn:ietf:any-string123",
+	{
+		urnTestData{
+			urnfield.Urn{
+				Nid: "ietf",
+				Nss: []string{"any-string123"},
+			},
+			"Good any string",
+			"urn:ietf:any-string123",
+			true,
+		},
+		Schema,
+	},
+}
 
 func TestSchema(t *testing.T) {
 	for _, u := range testIetfUrnData {
